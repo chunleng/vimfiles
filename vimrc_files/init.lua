95f88e9e6e9dcdeafd5350456a85eb28a5a1a976
Lim Chun Leng <limchunleng@gmail.com>
2022-02-26 14:31:50 +0900

    Scrolling for lsp documents

--- vimrc_files/init.lua
+++ vimrc_files/init.lua
@@ -491,6 +491,8 @@ require('packer').startup(function(use)
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
               },
+              ['<s-down>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+              ['<s-up>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
           },
           snippet = {
             expand = function(args)
