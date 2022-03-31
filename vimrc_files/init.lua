a4349c97d9dfab052fb400d55e107e554439ff80
Lim Chun Leng <limchunleng@gmail.com>
2022-03-31 23:49:20 +0900

    add LSP to CSS programming environment

--- vimrc_files/init.lua
+++ vimrc_files/init.lua
@@ -538,7 +538,7 @@ require('packer').startup(function(use)
             local servers = {
                 "tsserver", "tailwindcss", "eslint", "pyright", "efm", "jsonls",
                 "sumneko_lua", "jdtls", "vimls", "html", "yamlls",
-                "terraformls", "tflint", "cssls"
+                "terraformls", "tflint", "cssls", "cssmodules_ls", "dockerls"
             }
             -- Loop through the servers listed above and set them up. If a server is
             -- not already installed, install it.
