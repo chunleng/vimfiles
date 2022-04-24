793ad92644a65bdc74e44947b5b7e70107447f14
Lim Chun Leng <limchunleng@gmail.com>
2022-04-24 19:36:24 +0900

    remove grammarly as it has been really buggy after neovim 0.7

--- vimrc_files/lua/config/lsp-installer.lua
+++ vimrc_files/lua/config/lsp-installer.lua
@@ -7,8 +7,7 @@ function M.setup()
     local servers = {
         "tsserver", "tailwindcss", "eslint", "pyright", "efm", "jsonls",
         "sumneko_lua", "jdtls", "vimls", "html", "yamlls", "terraformls",
-        "tflint", "cssls", "cssmodules_ls", "dockerls", "grammarly",
-        "solargraph"
+        "tflint", "cssls", "cssmodules_ls", "dockerls", "solargraph"
     }
     -- Loop through the servers listed above and set them up. If a server is
     -- not already installed, install it.
