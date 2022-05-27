92b47cfc480a1e969eb30609055260a8b8083914
Lim Chun Leng <lim@logly.co.jp>
2022-05-27 18:16:31 +0900

    update copilot to use node 16

--- vimrc_files/lua/config/copilot.lua
+++ vimrc_files/lua/config/copilot.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.setup()
     vim.g.copilot_no_tab_map = 1
+    vim.g.copilot_node_command = "~/.asdf/installs/nodejs/lts-gallium/bin/node"
     vim.g.copilot_filetypes = {DressingInput = false}
     vim.api.nvim_set_keymap("i", "<c-space>", "copilot#Accept(\"\\<CR>\")",
                             {silent = true, script = true, expr = true})
