ad2ca6e07570eb51fe41c1ceb320b4a02fcde65e
Lim Chun Leng <lim@logly.co.jp>
2022-06-03 22:09:31 +0900

    fixup! update nvim-tree settings

--- vimrc_files/lua/config/nvim-tree.lua
+++ vimrc_files/lua/config/nvim-tree.lua
@@ -3,19 +3,6 @@ local M = {}
 function M.setup()
     vim.api.nvim_set_keymap("n", "<leader><space>", "<cmd>NvimTreeFindFile<cr>",
                             {noremap = true, silent = true})
-    -- vim.g.nvim_tree_group_empty = 1
-    -- vim.g.nvim_tree_git_hl = 1
-    -- vim.g.nvim_tree_icons = {
-    --     git = {
-    --         unstaged = "",
-    --         staged = "",
-    --         unmerged = "",
-    --         renamed = "→",
-    --         untracked = "?",
-    --         deleted = "",
-    --         ignored = "◌"
-    --     }
-    -- }
     require'nvim-tree'.setup {
         disable_netrw = false,
         open_on_setup = true,
