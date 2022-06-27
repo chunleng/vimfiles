ab5f7a763d4283399c102bf87f5ea0bff7ace736
Lim Chun Leng <lim@logly.co.jp>
2022-06-27 16:08:52 +0900

    fixup! add preferred indent settings

--- scripts/common-utils/lua/common-utils/init.lua
+++ scripts/common-utils/lua/common-utils/init.lua
@@ -34,8 +34,8 @@ local function configure_preferred_settings()
     vim.o.scrolloff = 5
     vim.o.wrap = false
     vim.o.termguicolors = true
-    vim.o.shiftwidth = 2
-    vim.o.tabstop = 2
+    vim.o.shiftwidth = 4
+    vim.o.tabstop = 4
     vim.o.expandtab = true
 
     vim.g.mapleader = " "
