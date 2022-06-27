edeeda88c37b7c4ac26720ecec618340c74d8f9e
Lim Chun Leng <lim@logly.co.jp>
2022-06-27 16:08:25 +0900

    exclude NvimTree from indent guide

--- vimrc_files/lua/config/indent-guides.lua
+++ vimrc_files/lua/config/indent-guides.lua
@@ -6,7 +6,8 @@ function M.setup()
         indent_guide_size = 100,
         indent_tab_guides = true,
         exclude_filetypes = {
-            'WhichKey', 'markdown', 'aerial', 'dashboard', 'help', 'fzf'
+            'WhichKey', 'markdown', 'aerial', 'dashboard', 'help', 'fzf',
+            "NvimTree"
         },
         odd_colors = {fg = base16.colors.base03, bg = base16.colors.base00_15},
         even_colors = {fg = base16.colors.base03, bg = base16.colors.base00}
