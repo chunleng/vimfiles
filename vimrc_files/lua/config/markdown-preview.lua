e78d1a2aec97dac378aab282035b69393e852f41
Lim Chun Leng <limchunleng@gmail.com>
2022-10-14 18:02:43 +0900

    Add plantuml to markdown-preview

--- vimrc_files/lua/config/markdown-preview.lua
+++ vimrc_files/lua/config/markdown-preview.lua
@@ -1,8 +1,9 @@
 local M = {}
 
 function M.setup()
-    vim.cmd "autocmd FileType markdown nnoremap <silent><buffer><leader>tm :MarkdownPreviewToggle<cr>"
+    vim.cmd "autocmd FileType markdown,plantuml nnoremap <silent><buffer><leader>tm :MarkdownPreviewToggle<cr>"
     vim.g.mkdp_auto_close = 0
+    vim.g.mkdp_filetypes = {'markdown', 'plantuml'}
 end
 
 return M
