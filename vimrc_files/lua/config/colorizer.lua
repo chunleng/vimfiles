041874b59e1dba0c6b8b3d18b9f09a751837381f
Lim Chun Leng <limchunleng@gmail.com>
2022-12-14 16:19:10 +0900

    Fix colorizer name disabling setup

--- vimrc_files/lua/config/colorizer.lua
+++ vimrc_files/lua/config/colorizer.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
     vim.api.nvim_set_keymap("n", "<leader>tc", ":ColorizerToggle<cr>",
                             {silent = true})
-    require'colorizer'.setup({}, {names = false})
+    require'colorizer'.setup({user_default_options = {names = false}})
 end
 
 return M
