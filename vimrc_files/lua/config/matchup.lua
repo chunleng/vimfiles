283276c6425fda94b00dd12e600b5b843ecbc892
Lim Chun Leng <limchunleng@gmail.com>
2022-12-28 14:53:31 +0900

    fix: adjust MatchParen bg

--- vimrc_files/lua/config/matchup.lua
+++ vimrc_files/lua/config/matchup.lua
@@ -3,8 +3,7 @@ local M = {}
 function M.setup()
     vim.g.matchup_matchparen_offscreen = {method = ""}
     local theme = require('common-theme')
-    theme.set_hl('MatchParen',
-                 {fg = 1, bg = theme.blender.bg_lighter_1, bold = true})
+    theme.set_hl('MatchParen', {fg = 1, bold = true})
     theme.set_hl('MatchWord', {bg = theme.blender.bg_lighter_1, bold = true})
 end
 
