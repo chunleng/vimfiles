3f60066aba6e2028c37de36c0617661e5f6c68e8
Lim Chun Leng <limchunleng@gmail.com>
2022-12-29 17:18:25 +0900

    fix: bug with the demote and promote function

--- vimrc_files/lua/config/bullets.lua
+++ vimrc_files/lua/config/bullets.lua
@@ -7,12 +7,12 @@ function M.setup()
     vim.g.bullets_set_mappings = 0
     vim.g.bullets_custom_mappings = {
         {'nnoremap', 'o', '<plug>(bullets-newline)'},
-        {'inoremap', '<c-d>', '<plug>(bullets-demote)'},
-        {'nnoremap', '<<', '<plug>(bullets-demote)'},
-        {'vnoremap', '<', '<plug>(bullets-demote)'},
-        {'inoremap', '<c-t>', '<plug>(bullets-promote)'},
-        {'nnoremap', '>>', '<plug>(bullets-promote)'},
-        {'vnoremap', '>', '<plug>(bullets-promote)'}
+        {'inoremap', '<c-d>', '<plug>(bullets-promote)'},
+        {'nnoremap', '<<', '<plug>(bullets-promote)'},
+        {'vnoremap', '<', '<plug>(bullets-promote)'},
+        {'inoremap', '<c-t>', '<plug>(bullets-demote)'},
+        {'nnoremap', '>>', '<plug>(bullets-demote)'},
+        {'vnoremap', '>', '<plug>(bullets-demote)'}
     }
 end
 
