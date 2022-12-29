e514d3d76faa34c33feca3e65fb1c49c7ff5f229
Lim Chun Leng <limchunleng@gmail.com>
2022-12-30 01:55:17 +0900

    chore: move vim snippets to luasnip

--- vimrc_files/snippets/lua/template.lua
+++ vimrc_files/snippets/lua/template.lua
@@ -30,6 +30,67 @@ table.insert(M,
 
 	return M
 ]], {i(0)})))
+
+table.insert(M,
+             s({trig = 'template/lua/vimtest', dscr = 'Template for Vimtest'},
+               fmta([[
+	-- https://github.com/vim-test/vim-test#features
+	vim.g['test#<>#runner'] = '<>'
+	vim.g['test#<>#executable'] = '<>'
+]], {
+    i(1, '<language>'), i(2, '<runner>'), l(l._1 .. '#' .. l._2, {1, 2}),
+    i(3, '<path/to/executable>')
+})))
+
+table.insert(M, s({
+    trig = 'template/lua/autoformat_on_save',
+    dscr = 'Template for invoking format on save'
+}, fmta([[
+	local group_name = 'LspAutoformat'
+	vim.api.nvim_create_augroup(group_name, {clear = true})
+	vim.api.nvim_create_autocmd('BufWritePre', {
+		buffer = 0,
+		callback = function() vim.lsp.buf.format({timeout_ms=5000}) end,
+		group = group_name
+	})
+]], {})))
+
+table.insert(M,
+             s(
+                 {
+        trig = 'template/dadbod/db_setup',
+        dscr = 'Template for Vim Dadbod Setup'
+    }, fmta([[
+	-- :h dadbod
+	vim.g.dbs = {
+		{ name = "dev-pg", url = "postgres://postgres:password@localhost:5432/db"},
+		{ name = "dev-mysql", url = "mysql://mysql:password@127.0.0.1:3306/db?default-character-set=utf8"}
+	}
+]], {})))
+
+table.insert(M,
+             s({trig = 'template/dap/python', dscr = 'Template for DAP Python'},
+               fmta([[
+	require('dap').configurations.python = {
+		{
+			type = 'python',
+			request = 'launch',
+			name = 'Launch current',
+			program = '${file}', -- current file
+			console = 'externalTerminal',
+			pythonPath = os.getenv('VIRTUAL_ENV') .. '/bin/python'
+		},
+		{
+			type = 'python',
+			request = 'launch',
+			name = 'Test current',
+			code = 'import pytest; pytest.main(["${file}"])',
+			console = 'externalTerminal',
+			pythonPath = os.getenv('VIRTUAL_ENV') .. '/bin/python'
+		}
+	}
+]], {})))
+
 return M
 
 -- vim: noet
