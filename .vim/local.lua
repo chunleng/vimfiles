05ab4c8a119d86a75bec28bb516ec99f619cc964
Lim Chun Leng <limchunleng@gmail.com>
2022-12-30 01:56:20 +0900

    chore: convert vim/local.lua to lua

--- .vim/local.lua
+++ .vim/local.lua
@@ -1,11 +1,20 @@
-vim.cmd [[
-augroup LspFormatting
-    autocmd! * <buffer>
-    autocmd BufWritePre <buffer> lua vim.lsp.buf.format({timeout_ms=5000})
-augroup END
+local group_name = 'SaveAction'
+vim.api.nvim_create_augroup(group_name, {clear = true})
+vim.api.nvim_create_autocmd('BufWritePre', {
+    buffer = 0,
+    callback = function() vim.lsp.buf.format({timeout_ms = 5000}) end,
+    group = group_name
+})
+vim.api.nvim_create_autocmd('BufWritePost', {
+    pattern = '*.lua',
+    command = 'source <afile> | PackerCompile',
+    group = group_name
+})
+-- :h dadbod
 
-augroup packer_user_config
-  autocmd!
-  autocmd BufWritePost *.lua source <afile> | PackerCompile
-augroup end
-]]
+vim.g.dbs = {
+    {name = "dev-pg", url = "postgres://postgres:password@localhost:5432/db"}, {
+        name = "dev-mysql",
+        url = "mysql://mysql:password@127.0.0.1:3306/db?default-character-set=utf8"
+    }
+}
