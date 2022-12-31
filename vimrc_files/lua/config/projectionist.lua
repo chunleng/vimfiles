f62c5f91fa5c08f863b8f50510071d02eaf61e10
Lim Chun Leng <limchunleng@gmail.com>
2022-12-31 14:32:53 +0900

    fix: only call alternate file if alternate file exists

--- vimrc_files/lua/config/projectionist.lua
+++ vimrc_files/lua/config/projectionist.lua
@@ -2,7 +2,9 @@ local M = {}
 
 function M.setup()
     local utils = require('common-utils')
-    utils.noremap('n', '<c-s>', '<cmd>A<cr>')
+    utils.noremap('n', '<c-s>', function()
+        if vim.fn.exists(':A') == 2 then vim.cmd('A') end
+    end)
 end
 
 return M
