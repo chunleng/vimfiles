0f05ea3222c085490b23a5eab37f4223e14c3887
Lim Chun Leng <limchunleng@gmail.com>
2023-01-03 22:10:47 +0900

    chore: move json snippet to luasnip

---
+++ vimrc_files/snippets/json/template.lua
@@ -0,0 +1,75 @@
+local M = {}
+
+table.insert(M, s({
+    trig = 'template/typescriptreact/tsconfig',
+    dscr = 'Template for tsconfig.json'
+}, fmta([[
+	// For TypeScript 4.4.3
+	{
+		"compilerOptions": {
+			"target": "es5",
+			"baseUrl": "./",
+			"module": "esnext",
+			"moduleResolution": "node",
+
+			"removeComments": true,
+			"esModuleInterop": true,
+			"forceConsistentCasingInFileNames": true,
+			"jsx": "react-jsx",
+
+			"strict": true,
+
+			"skipLibCheck": true,
+			"resolveJsonModule": true,
+			"allowJs": true,
+			"noEmit": true,
+			"isolatedModules": true,
+			"allowSyntheticDefaultImports": true,
+			"lib": ["dom", "dom.iterable", "esnext"]
+		},
+		"include": ["**/*.ts", "**/*.tsx"${1:, ...}],
+		"exclude": ["node_modules"]
+	}
+]], {})))
+
+table.insert(M, s({
+    trig = 'template/typescriptreact/eslint',
+    dscr = 'Template for TypeScript React ESLint'
+}, fmta([[
+	// `yarn add -D` or `npm i --save-dev`
+	//   eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-jsx-a11y
+	// `yarn add -D -E` or `npm i --save-dev -E`
+	//   prettier
+	{
+		"root": true,
+		"env": {
+			"browser": true,
+			"es2021": true
+		},
+		"extends": [
+			"eslint:recommended",
+			"plugin:react/recommended",
+			"plugin:react/jsx-runtime",
+			"plugin:react-hooks/recommended",
+			"plugin:jsx-a11y/recommended",
+			"plugin:@typescript-eslint/eslint-recommended",
+			"plugin:@typescript-eslint/recommended",
+			"plugin:@typescript-eslint/recommended-requiring-type-checking",
+			"plugin:prettier/recommended"
+		],
+		"parser": "@typescript-eslint/parser",
+		"parserOptions": {
+			"ecmaFeatures": {
+				"jsx": true
+			},
+			"ecmaVersion": 12,
+			"sourceType": "module",
+			"project": "./tsconfig.json"
+		},
+		"plugins": [],
+		"rules": {}
+	}
+]], {})))
+
+return M
+-- vim: noet
