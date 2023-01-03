efb1900452cf147d0dcb03d50375a706b2900e47
Lim Chun Leng <limchunleng@gmail.com>
2023-01-04 00:03:06 +0900

    feat: emmet add visual mode emmet

--- vimrc_files/lua/config/emmet.lua
+++ vimrc_files/lua/config/emmet.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 function M.setup()
-    vim.g.user_emmet_install_global = 0
+    vim.g.user_emmet_install_global = 1
+    vim.g.emmet_install_only_plug = 1
 
     local utils = require('common-utils')
-    utils.noremap("i", "<c-;>", "<c-o>dd<c-o>O<c-o>:Emmet <c-r>\"<cr>")
-    utils.noremap("n", "<c-;>", ":Emmet ", false)
+    utils.noremap({'i', 'v'}, '<c-;>', '<plug>(emmet-expand-abbr)')
+    utils.noremap('n', '<c-;>', ':Emmet ', false)
 end
 
 return M
