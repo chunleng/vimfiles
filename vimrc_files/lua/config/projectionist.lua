f0cda78c6e747f2ccdba6d9a0c200770fb6e3059
Lim Chun Leng <chunleng.lim@woven-planet.global>
2023-01-05 10:55:38 +0900

    fix: projectionist error message when there is no alternative

--- vimrc_files/lua/config/projectionist.lua
+++ vimrc_files/lua/config/projectionist.lua
@@ -3,7 +3,10 @@ local M = {}
 function M.setup()
     local utils = require('common-utils')
     utils.noremap('n', '<c-s>', function()
-        if vim.fn.exists(':A') == 2 then vim.cmd('A') end
+        if vim.fn.exists(':A') == 2 then
+            xpcall(function(var_a) vim.cmd('A') end,
+                   function(err) print('No alternative file') end)
+        end
     end)
 end
 
