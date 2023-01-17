9b0b941fa1a5464e7655e542599566edd0e356a9
Lim Chun Leng <limchunleng@gmail.com>
2023-01-17 14:58:20 +0900

    fix: remove unnecessary vimlocal

--- .vim/local.lua
+++ .vim/local.lua
@@ -10,11 +10,3 @@ vim.api.nvim_create_autocmd('BufWritePost', {
     command = 'source <afile> | PackerCompile',
     group = group_name
 })
--- :h dadbod
-
-vim.g.dbs = {
-    {name = "dev-pg", url = "postgres://postgres:password@localhost:5432/db"}, {
-        name = "dev-mysql",
-        url = "mysql://mysql:password@127.0.0.1:3306/db?default-character-set=utf8"
-    }
-}
