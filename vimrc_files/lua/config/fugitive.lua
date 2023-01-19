e4764e36dfe9a5413d2b11a4502bc7261b2be292
Lim Chun Leng <limchunleng@gmail.com>
2023-01-20 01:52:24 +0900

    feat: fugitive menu and better highlight

--- vimrc_files/lua/config/fugitive.lua
+++ vimrc_files/lua/config/fugitive.lua
@@ -3,14 +3,36 @@ local M = {}
 function M.setup()
     local utils = require('common-utils')
     utils.noremap("n", "<leader>gf", "<cmd>GBrowse<cr>")
-    utils.noremap("n", "<leader>gb", '<cmd>Git blame<cr>')
+    utils.noremap("n", "<c-s-g>", function()
+        vim.ui.select({'File Blame', 'File Log'}, {}, function(choice)
+            if choice == 'File Blame' then
+                vim.cmd('Git blame --date=short')
+            elseif choice == 'File Log' then
+                vim.cmd('vert Git log %')
+            end
+        end)
+    end)
 
     vim.cmd [[
         augroup FugitiveFiletype
             autocmd!
-            autocmd FileType fugitiveblame nnoremap <buffer> q <cmd>bd<cr>
+            autocmd FileType fugitiveblame,git nnoremap <buffer> q <cmd>bd<cr>
+            autocmd FileType fugitiveblame,git nnoremap <buffer> <c-s-g> <cmd>bd<cr>
+            autocmd FileType fugitiveblame setlocal nolist
         augroup END
     ]]
+
+    local theme = require('common-theme')
+    theme.set_hl('FugitiveblameDelimiter', {fg = 0})
+    theme.set_hl('FugitiveblameUncommitted', {fg = 0})
+    theme.set_hl('gitIdentity', {fg = 2})
+    theme.set_hl('diffOldFile', {fg = theme.blender.fg_darker_3})
+    theme.set_hl('diffRemoved', {fg = theme.blender.fg_darker_3})
+    theme.set_hl('diffNewFile', {fg = theme.blender.add})
+    theme.set_hl('diffAdded', {fg = theme.blender.add})
+    theme.set_hl('diffFile',
+                 {fg = 4, bg = theme.blender.bg_lighter_1, bold = true})
+    theme.set_hl('diffLine', {fg = 6, underline = true})
 end
 
 return M
--- vimrc_files/lua/config/trailing-whitespace.lua
+++ vimrc_files/lua/config/trailing-whitespace.lua
@@ -3,7 +3,8 @@ local M = {}
 function M.setup()
     vim.g.extra_whitespace_ignored_filetypes = {
         "Mundo", "MundoDiff", "aerial", "dashboard", "dbout", "dbui", "fzf",
-        "lspinfo", "help", 'mason', "", 'neotest-summary'
+        "lspinfo", "help", 'mason', "", 'neotest-summary', 'fugitiveblame',
+        'git'
     }
 end
 
