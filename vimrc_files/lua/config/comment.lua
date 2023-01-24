34d4420582f06654ced82bfd6222bb179b1e2c50
Lim Chun Leng <limchunleng@gmail.com>
2023-01-24 12:20:07 +0900

    feat: make comment stick

--- vimrc_files/lua/config/comment.lua
+++ vimrc_files/lua/config/comment.lua
@@ -1,7 +1,23 @@
 local M = {}
 
 function M.setup()
-    require('Comment').setup({mappings = {basic = false, extra = false}})
+    require('Comment').setup({
+        sticky = false,
+        mappings = {basic = false, extra = false},
+        pre_hook = function()
+            M.count = #vim.api.nvim_get_current_line()
+            M.cursor_col = vim.api.nvim_win_get_cursor(0)[2]
+        end,
+        post_hook = function()
+            local cnt_change = #vim.api.nvim_get_current_line() - M.count
+            local cursor = vim.api.nvim_win_get_cursor(0)
+            local cursor_change = cursor[2] - M.cursor_col
+
+            cursor[2] = cursor[2] + cnt_change - cursor_change
+            vim.api.nvim_win_set_cursor(0, cursor)
+        end
+    })
+
     local api = require('Comment.api')
     local utils = require('common-utils')
     utils.keymap({'i', 'n'}, '<c-/>', api.toggle.linewise.current)
