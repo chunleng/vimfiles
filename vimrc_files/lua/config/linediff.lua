351f2ac3c400421cec9ed1bb6c288cb7cbddf9c8
Lim Chun Leng <limchunleng@gmail.com>
2023-01-30 13:10:25 +0900

    fix: visual linediff not working

--- vimrc_files/lua/config/linediff.lua
+++ vimrc_files/lua/config/linediff.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
     local utils = require('common-utils')
     -- :Linediff<cr> not <cmd> because we want to pick up the range
-    utils.keymap("x", "<leader>d", "<cmd>Linediff<cr>")
+    utils.keymap("x", "<leader>d", ":Linediff<cr>")
 end
 
 return M
