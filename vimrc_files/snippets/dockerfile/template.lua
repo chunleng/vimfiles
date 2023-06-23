0962a3b9dc6ef9d73eb8529074db4b2c90ab98a1
Lim Chun Leng <limchunleng@gmail.com>
2023-06-23 18:02:49 +0900

    chore: migrate dockerfile to Luasnip

---
+++ vimrc_files/snippets/dockerfile/template.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+table.insert(M,
+             s({trig = '?apt/install', dscr = 'Apt update and install'}, fmta(
+                   [[
+RUN apt-get update && \
+    apt-get install -y <> && \
+    rm -rf /var/lib/apt/lists/*
+]], {i(1, 'curl')})))
+
+table.insert(M, s({trig = '?apk/install', dscr = 'Alpine apk install'}, fmta([[
+RUN apk add --no-cache <>
+]], {i(1, 'curl')})))
+
+table.insert(M, s({trig = '?poetry/install', dscr = 'Poetry install'}, fmta([[
+RUN pip install poetry && \
+    poetry install && \
+    rm -rf ~/.cache
+]], {})))
+
+return M
+-- vim: noet
