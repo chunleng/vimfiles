9d08bfd6fa293a17eb207d22710c34db8e32c2af
Lim Chun Leng <limchunleng@gmail.com>
2023-07-26 22:29:29 +0900

    chore: luasnip migrate docker-compose template

---
+++ vimrc_files/snippets/yaml/template_docker_compose.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+table.insert(M,
+             s(
+                 {
+        trig = '?docker_compose/init',
+        dscr = 'Template for docker-compose init'
+    }, fmta([[
+	services:
+		web:
+			image: <>
+			ports:
+				- "<>"
+			volumes:
+				- <>
+		<>
+]], {
+        i(1, 'imagename:version'), i(2, 'host:docker'),
+        i(3, 'local_path:docker_path'), i(0)
+    })))
+
+return M
+-- vim: noet
