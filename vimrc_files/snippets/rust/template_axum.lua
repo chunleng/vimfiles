817aab3fc579583d50c74dab5e3dbf1135880b48
Lim Chun Leng <limchunleng@gmail.com>
2023-10-09 21:49:38 +0900

    chore: rust snippet

---
+++ vimrc_files/snippets/rust/template_axum.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+table.insert(M,
+             s({trig = '?axum/init', dscr = 'Template for Axum Init'}, fmta([[
+	use axum::{routing::get, Router};
+
+	#[tokio::main]
+	async fn main() {
+		let app = Router::new().route("/", get(|| async { "Hello world" }));
+		axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+			.serve(app.into_make_service())
+			.await
+			.unwrap();
+	}
+]], {})))
+
+return M
+-- vim: noet
---
+++ vimrc_files/snippets/rust/template_leptos.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+table.insert(M,
+             s({trig = '?leptos/init', dscr = 'Template for Leptos Init'},
+               fmta([[
+	use leptos::*;
+
+	fn main() {
+		mount_to_body(|cx| view! { cx,  <<p>>"Hello, world!"<</p>> })
+	}
+]], {})))
+
+table.insert(M,
+             s(
+                 {
+        trig = '?leptos/component',
+        dscr = 'Template for Leptos component'
+    }, fmta([[
+	#[component]
+	fn <>(cx: Scope) ->> impl IntoView {
+		view! { cx, <> }
+	}
+]], {i(1, "ComponentName"), i(0, "<p>\"Foo\"</p>")})))
+
+table.insert(M,
+             s(
+                 {
+        trig = '?leptos/signal',
+        dscr = 'Template for creating Leptos signal'
+    }, fmta([[
+	let (<>, set_<>) = create_signal(cx, <>);
+]], {i(1, "count"), l(l._1, 1), i(2, '"foo".to_string()')})))
+
+return M
+-- vim: noet
