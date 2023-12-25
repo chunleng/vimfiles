f2c73f77d457c033e63a4f73ffa2cdbcbf664337
Lim Chun Leng <limchunleng@gmail.com>
2023-12-25 23:27:59 +0900

    refactor: refactor gpt prompt function to local function with default values
    - prompt function
    - remove insert mode autocomplete

--- vimrc_files/lua/config/ai.lua
+++ vimrc_files/lua/config/ai.lua
@@ -1,3 +1,5 @@
+local RangeType = {SELECTION = 0, ALL_BEFORE = 1}
+local gp = require("gp")
 local M = {
     models = {programmer = {model = 'gpt-4', temperature = 0.5, top_p = 1}},
     system_prompts = {
@@ -8,80 +10,75 @@ local M = {
     }
 }
 
+local function send(template, table)
+    local range = nil
+    if table.range_type == RangeType.ALL_BEFORE then
+        local current_line = vim.api.nvim_win_get_cursor(0)[1]
+        range = current_line == 1 and {} or
+                    {range = 2, line1 = 1, line2 = current_line}
+    else
+        range = {
+            range = 2,
+            line1 = vim.api.nvim_buf_get_mark(0, "<")[1],
+            line2 = vim.api.nvim_buf_get_mark(0, ">")[1]
+        }
+    end
+    local target = table.target or gp.Target.append
+    local prompt_text = table.has_prompt and "󱚤  ~" or nil
+    local model = table.model or M.models.programmer
+    local system_prompt = table.system_prompt or
+                              M.system_prompts.programmer_code
+    gp.Prompt(range, target, prompt_text, model, template, system_prompt)
+end
+
 function M.setup()
-    local gp = require("gp")
     gp.setup({command_prompt_prefix_template = "󱚤  {{agent}} ~"})
 
     local utils = require('common-utils')
     utils.keymap('n', '<c-space>', function()
         vim.ui.select({'Programmer: Complete Code'}, {}, function(choice)
             if choice == "Programmer: Complete Code" then
-                local template = "Having following from {{filename}}:\n\n" ..
-                                     "```{{filetype}}\n{{selection}}\n```\n\n" ..
-                                     "Please reply with only the code added.\n\n" ..
-                                     "Please continue the code with the following instruction: {{command}}"
-                gp.Prompt({
-                    range = 2,
-                    line1 = 1,
-                    line2 = vim.api.nvim_win_get_cursor(0)[1]
-                }, gp.Target.append, gp.config.command_prompt_prefix_template,
-                          M.models.programmer, template,
-                          M.system_prompts.programmer_code)
+                send("Having following from {{filename}}:\n\n" ..
+                         "```{{filetype}}\n{{selection}}\n```\n\n" ..
+                         "Please reply with only the code added.\n\n" ..
+                         "Please continue the code with the following instruction: {{command}}",
+                     {has_prompt = true, range_type = RangeType.ALL_BEFORE})
             end
         end)
     end, {silent = false})
-    utils.keymap('i', '<c-space>', function()
-        vim.cmd("stopinsert")
-        local template = "Having following from {{filename}}:\n\n" ..
-                             "```{{filetype}}\n{{selection}}\n```\n\n" ..
-                             "Please reply with only the code added.\n\n" ..
-                             "Please continue the code with what you think comes after."
-        gp.Prompt({
-            range = 2,
-            line1 = 1,
-            line2 = vim.api.nvim_win_get_cursor(0)[1]
-        }, gp.Target.append, nil, M.models.programmer, template,
-                  M.system_prompts.programmer_code)
-
-    end)
     utils.keymap('v', '<c-space>', function()
         vim.ui.select({
             'Programmer: Refine Code', 'Programmer: Summarize Code',
             'Programmer: Code Review', 'Programmer: Write Unit Test'
         }, {}, function(choice)
-            local selection_range = {
-                range = 2,
-                line1 = vim.api.nvim_buf_get_mark(0, "<")[1],
-                line2 = vim.api.nvim_buf_get_mark(0, ">")[1]
-            }
             if choice == 'Programmer: Refine Code' then
-                local template = "Having following from {{filename}}:\n\n" ..
-                                     "```{{filetype}}\n{{selection}}\n```\n\n" ..
-                                     "Please rewrite the code with the following instruction:\n\n{{command}}"
-                gp.Prompt(selection_range, gp.Target.rewrite, "󱚤 ~",
-                          M.models.programmer, template,
-                          M.system_prompts.programmer_code)
+                send("Having following from {{filename}}:\n\n" ..
+                         "```{{filetype}}\n{{selection}}\n```\n\n" ..
+                         "Please rewrite the code with the following instruction:\n\n{{command}}",
+                     {target = gp.Target.rewrite})
             elseif choice == 'Programmer: Write Unit Test' then
-                local template = "Having following from {{filename}}:\n\n" ..
-                                     "```{{filetype}}\n{{selection}}\n```\n\n" ..
-                                     "Please write unit test to test if the code is working"
-                gp.Prompt(selection_range, gp.Target.enew, nil,
-                          M.models.programmer, template,
-                          M.system_prompts.programmer_code)
+                send("Having following from {{filename}}:\n\n" ..
+                         "```{{filetype}}\n{{selection}}\n```\n\n" ..
+                         "Please write unit test to test if the code is working",
+                     {target = gp.Target.enew})
             elseif choice == 'Programmer: Summarize Code' then
-                local template = "Having following from {{filename}}:\n\n" ..
-                                     "```{{filetype}}\n{{selection}}\n```\n\n" ..
-                                     "Please understand what the code is trying to do respond with a summary."
-                gp.Prompt(selection_range, gp.Target.popup, nil,
-                          M.models.programmer, template,
-                          M.system_prompts.programmer_chat)
+                send("Having following from {{filename}}:\n\n" ..
+                         "```{{filetype}}\n{{selection}}\n```\n\n" ..
+                         "Please understand what the code is trying to do and " ..
+                         "respond with a summary the core logic in steps but as short as possible.\n\n" ..
+                         "It's okay to drop steps which are not relevant to understand the logic",
+                     {
+                    target = gp.Target.popup,
+                    system_prompt = M.system_prompts.programmer_chat
+                })
             elseif choice == 'Programmer: Code Review' then
-                local template = "Having following from {{filename}}:\n\n" ..
-                                     "```{{filetype}}\n{{selection}}\n```\n\n" ..
-                                     "Please analyze for code smells and suggest improvements."
-                gp.Prompt(selection_range, gp.Target.popup, nil,
-                          M.models.programmer, template,
-                          M.system_prompts.programmer_chat)
+                send("Having following from {{filename}}:\n\n" ..
+                         "```{{filetype}}\n{{selection}}\n```\n\n" ..
+                         "Please analyze for code smells and suggest improvements.",
+                     {
+                    target = gp.Target.popup,
+                    system_prompt = M.system_prompts.programmer_chat
+                })
             end
         end)
     end)
