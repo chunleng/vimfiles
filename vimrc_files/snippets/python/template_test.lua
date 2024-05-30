976002d2289cb41d9c0b4b0856149fae70fbc11a
Lim Chun Leng <limchunleng@gmail.com>
2024-05-30 13:16:25 +0900

    chore: add unit test snippet for python

--- vimrc_files/snippets/python/template_test.lua
+++ vimrc_files/snippets/python/template_test.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+local function get_file_upper_camel(_, snip)
+    return sn(nil, {
+        i(1, require('utils').snake_to_upper_camel(
+              snip.env.TM_FILENAME:match("^(.+)%..+$")))
+    })
+end
+
 table.insert(M,
              s(
                  {
@@ -21,5 +28,28 @@ table.insert(M, s({
 			yield mck
 ]], {i(1, 'foo'), i(0, 'path.to.code')})))
 
+table.insert(M,
+             s(
+                 {
+        trig = '?python/unittest/class',
+        dscr = 'Template for Unittest'
+    }, fmta([[
+	class <>:
+		def setup_method(self):
+			<>
+
+		def tear_method(self):
+			pass
+]], {d(1, get_file_upper_camel, {}), i(0, 'pass')})))
+
+table.insert(M,
+             s(
+                 {
+        trig = '?python/unittest/function',
+        dscr = 'Template for Unittest'
+    }, fmta([[
+	def test_<>():
+		<>
+]], {i(1, 'func'), i(0, 'pass')})))
 return M
 -- vim: noet
