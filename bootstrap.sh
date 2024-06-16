563f178fecba8d0b68fddedde3380341a6198d43
Lim Chun Leng <limchunleng@gmail.com>
2024-06-16 20:29:58 +0900

    chore: check in lazy-lock.json file

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,11 @@ APP_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 # install vimrc
 mkdir -p $HOME/.config/nvim
-ln -sn ${APP_PATH}/vimrc_files/init.lua $HOME/.config/nvim/
-ln -sn ${APP_PATH}/vimrc_files/lua $HOME/.config/nvim/lua
-ln -sn ${APP_PATH}/vimrc_files/UltiSnips $HOME/.config/nvim/UltiSnips
-ln -sn ${APP_PATH}/vimrc_files/snippets $HOME/.config/nvim/snippets
+ln -sfn ${APP_PATH}/vimrc_files/init.lua $HOME/.config/nvim/
+ln -sfn ${APP_PATH}/vimrc_files/lua $HOME/.config/nvim/lua
+ln -sfn ${APP_PATH}/lazy-lock.json $HOME/.config/nvim/
+ln -sfn ${APP_PATH}/vimrc_files/UltiSnips $HOME/.config/nvim/UltiSnips
+ln -sfn ${APP_PATH}/vimrc_files/snippets $HOME/.config/nvim/snippets
 
 test -d ~/.local/share/nvim/lazy/lazy.nvim || \
     git clone --filter=blob:none https://github.com/folke/lazy.nvim.git --branch=stable \
