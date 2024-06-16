655d019941d224269109d4f34c9fa87060c9a5d3
Lim Chun Leng <limchunleng@gmail.com>
2024-06-16 22:40:02 +0900

    chore: update gpt model

--- vimrc_files/lua/config/ai.lua
+++ vimrc_files/lua/config/ai.lua
@@ -1,15 +1,19 @@
 local RangeType = { SELECTION = 0, ALL_BEFORE = 1 }
 local gp = require("gp")
 local M = {
-	models = { programmer = { model = "gpt-4o", temperature = 0.5, top_p = 1 } },
+	-- Note:
+	-- * Temperature (0-1) adjust creativity with 0 (highly deterministic) and 1 (highly creative)
+	-- * Top P (0-1) adjust next word use. e.g. 0.1 will only use 10% of the commonly used words for the next token
+	models = {
+		programmer = { model = "gpt-4o", temperature = 0.1, top_p = 0.2 },
+		writing = { model = "gpt-4o", temperature = 0.8, top_p = 0.8 },
+	},
 	system_prompts = {
 		programmer_code = "I want you to act as an expert programmer.\n\n"
-			.. "Please avoid any commentary outside of the snippet response\n\n"
-			.. "Start and end your answer with:\n\n```",
+			.. "Strictly avoid any commentary outside of the snippet response\n\n"
+			.. "Start and end your answer with: ```",
 		programmer_chat = "I want you to act as an expert programmer.",
-		casual_writer = "I want you to act as a writer.\n\n"
-			.. "Please avoid any commentary outside of the response.\n\n"
-			.. "Use a more casual and friendly tone in your writing.",
+		casual_writer = "I want you to act as a writer.\n\n" .. "Use a more casual and friendly tone in your writing.",
 	},
 }
 
@@ -54,7 +58,7 @@ function M.setup()
 					"Having the following start:\n\n"
 						.. "```\n{{selection}}\n```\n\n"
 						.. "Please continue the writing with the following instruction: {{command}}",
-					{ has_prompt = true, range_type = RangeType.ALL_BEFORE }
+					{ has_prompt = true, range_type = RangeType.ALL_BEFORE, model = M.models.writing }
 				)
 			end
 		end)
@@ -118,7 +122,7 @@ function M.setup()
 					"Having the following:\n\n"
 						.. "```\n{{selection}}\n```\n\n"
 						.. "Please rewrite it with the following instruction: {{command}}",
-					{ has_prompt = true, target = gp.Target.rewrite }
+					{ has_prompt = true, target = gp.Target.rewrite, system_prompt = M.system_prompts.casual_writer }
 				)
 			end
 		end)
