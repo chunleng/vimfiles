252758da86cb70e204c9a47c377f4c9c6e0f1dd0
Lim Chun Leng <limchunleng@gmail.com>
2024-11-29 18:34:20 +0900

    chore: make vimrc local config load the correct amount of times

--- .vim/local.lua
+++ .vim/local.lua
@@ -1,3 +1,21 @@
+------------------------------
+-- Load every buffer change --
+------------------------------
+
+if vim.g.localvimrc_sourced_once_for_file == 1 then
+    return
+end
+-----------------------------
+-- Load once for each file --
+-----------------------------
+
+if vim.g.localvimrc_sourced_once == 1 then
+    return
+end
+--------------------------------
+-- Load once per vim instance --
+--------------------------------
+
 local group_name = 'SaveAction'
 vim.api.nvim_create_augroup(group_name, {clear = true})
 vim.api.nvim_create_autocmd('BufWritePre', {
