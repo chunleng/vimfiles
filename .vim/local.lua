6f4f2c201868768052af00f64aba5e19e13dcde6
Lim Chun Leng <limchunleng@gmail.com>
2025-03-31 01:37:51 +0900

    chore: update vimrc_local

--- .vim/local.lua
+++ .vim/local.lua
@@ -2,24 +2,28 @@
 -- Load every buffer change --
 ------------------------------
 
-local group_name = 'SaveAction'
-vim.api.nvim_create_augroup(group_name, {clear = true})
-vim.api.nvim_create_autocmd('BufWritePre', {
-    buffer = 0,
-    callback = function() vim.lsp.buf.format({timeout_ms = 5000}) end,
-    group = group_name
-})
-
 if vim.g.localvimrc_sourced_once_for_file == 1 then
-    return
+	return
 end
 -----------------------------
 -- Load once for each file --
 -----------------------------
 
 if vim.g.localvimrc_sourced_once == 1 then
-    return
+	return
 end
 --------------------------------
 -- Load once per vim instance --
 --------------------------------
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format({ timeout_ms = 5000 })
+	end,
+})
+
+require("neodev").setup()
+require("lspconfig").lua_ls.setup(require("config.lsp").default_setup.lua_ls)
+require("null-ls").setup({ sources = {
+	require("null-ls").builtins.formatting.stylua,
+} })
