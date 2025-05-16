f9c20aec6a324391cd8a13ba4049955661c901ba
Lim Chun Leng <limchunleng@gmail.com>
2025-05-16 11:17:24 +0900

    chore: add dioxus snippet

---
+++ vimrc_files/snippets/rust/template_dioxus.lua
@@ -0,0 +1,51 @@
+local M = {}
+
+table.insert(
+	M,
+	s(
+		{ trig = "----dioxus/init", dscr = "Template for Dioxus Init" },
+		fmta(
+			[[
+	use dioxus::prelude::*;
+
+	fn main() {
+		dioxus::launch(|| rsx! { "Hello World!" });
+	}
+
+]],
+			{}
+		)
+	)
+)
+
+table.insert(
+	M,
+	s(
+		{ trig = "----dioxus/component", dscr = "Template for dioxus component" },
+		fmta(
+			[[
+	#[component]
+	fn <>() ->> Element {
+		rsx! { <> }
+	}
+]],
+			{ i(2, "Func"), i(0) }
+		)
+	)
+)
+
+table.insert(
+	M,
+	s(
+		{ trig = "----dioxus/signal", dscr = "Template for dioxus signal" },
+		fmta(
+			[[
+	let <> = use_signal(|| <>);<>
+]],
+			{ i(1, "loading"), i(2, "false"), i(0) }
+		)
+	)
+)
+
+return M
+-- vim: noet
