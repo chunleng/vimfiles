9ac30bb586b7d6424ffbb0b5bf6c1dc6f5d93aed
Lim Chun Leng <limchunleng@gmail.com>
2026-02-09 15:14:16 +0900

    chore: add nvim-ufo to better manage folds

--- vimrc_files/init.lua
+++ vimrc_files/init.lua
@@ -826,4 +826,17 @@ require("lazy").setup({
 		"tiagovla/scope.nvim",
 		config = true,
 	},
+	{
+		-- Handle folding
+		-- https://github.com/kevinhwang91/nvim-ufo.git
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+		},
+		config = function()
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			require("ufo").setup()
+		end,
+	},
 })
