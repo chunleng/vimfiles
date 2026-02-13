0e6dd2230768f150e04b4b670e81471b24fe27b7
Lim Chun Leng <limchunleng@gmail.com>
2026-02-13 22:52:58 +0900

    chore: bootstrap default global mcp.json

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,7 @@ ln -sfn ${APP_PATH}/vimrc_files/lua $HOME/.config/nvim/lua
 ln -sfn ${APP_PATH}/lazy-lock.json $HOME/.config/nvim/
 ln -sfn ${APP_PATH}/vimrc_files/snippets $HOME/.config/nvim/snippets
 ln -sfn ${APP_PATH}/mason-lock.json $HOME/.config/nvim/
+ln -sfn ${APP_PATH}/mcp.json $HOME/.mcp.json
 
 test -d ${LAZY_INSTALL_DIR} || \
     git clone --filter=blob:none https://github.com/folke/lazy.nvim.git ${LAZY_INSTALL_DIR}
