91332e6f1b71203ef7d0fc4a438fbcbcf0c9d32b
Lim Chun Leng <limchunleng@gmail.com>
2026-02-16 22:24:16 +0900

    chore: add research action to codecompanion

--- vimrc_files/lua/plugins/ai/codecompanion.lua
+++ vimrc_files/lua/plugins/ai/codecompanion.lua
@@ -167,6 +167,65 @@ local function setup()
 			},
 			{
 				name = function()
+					return "Research"
+				end,
+				interaction = "chat",
+				description = "Research and learn about topic",
+				opts = {
+					rules = "default",
+					intro_message = "This chat is now preset to help you research on any topic",
+					modes = { "n" },
+				},
+				prompts = {
+					n = function(context)
+						local chat = codecompanion.chat({
+							auto_submit = false,
+							messages = {
+								{
+									role = "system",
+									content = [[The user will post question or topic of interest
+Your job is to search online and gather information for the user
+<guidelines>
+- Follow through links on the result if you think it can be helpful for the research
+- Summary of research should capture the essence of the research and you don't have to put too many details. The user can query later if needed
+- Use <outputFormat> when formatting the results of search, if not, you don't have to stick to the format
+</guidelines>
+<example>
+<userQuery>How to stay healthy?</userQuery>
+<outputFormat>
+### Sources
+
+#### 1. Search Query: How to stay healthy
+
+a. <source>
+b. <another source>
+..
+
+#### 2. Search Query: How to fall sick less frequently
+
+a. <sources>
+..
+
+### Summary
+
+(Provide result with source number [1a] to show where the sources came from. For `[1a]`, `1` is the query used and `a` is the source from that search)
+</outputFormat><example>]],
+								},
+								{
+									role = "user",
+									content = "What is...",
+								},
+							},
+						})
+						if chat then
+							chat.tool_registry:add_group("web", codecompanion_config.config.interactions.chat.tools)
+						end
+						return chat
+					end,
+				},
+			},
+			{
+				name = function()
 					return "Question"
 				end,
 				interaction = "chat",
@@ -176,7 +235,6 @@ local function setup()
 					intro_message = "This chat is now preset to help you answer questions you have in this directory",
 					modes = { "v" },
 				},
-				-- 				prompts = {
 				prompts = {
 					v = function(context)
 						-- NOTE: This doesn't work unless the following PR is merged: https://github.com/olimorris/codecompanion.nvim/pull/2762
