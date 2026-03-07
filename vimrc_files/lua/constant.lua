d06ebbfde56319846af9b4896d5d9d2d3dcfb24c
Lim Chun Leng <limchunleng@gmail.com>
2026-03-07 19:24:14 +0900

    chore: bump rust version to 1.94.0

--- vimrc_files/lua/constant.lua
+++ vimrc_files/lua/constant.lua
@@ -5,7 +5,7 @@ return {
 	NODEJS_PATH = vim.env.HOME .. "/.asdf/installs/nodejs/" .. nodejs_version .. "/bin",
 	NODEJS_MODULES = vim.env.HOME .. "/.asdf/installs/nodejs/" .. nodejs_version .. "/lib/node_modules",
 	PYTHON_PATH = vim.env.HOME .. "/.asdf/installs/python/3.12.11/bin",
-	RUST_PATH = vim.env.HOME .. "/.asdf/installs/rust/1.88.0/toolchains/1.88.0-aarch64-apple-darwin/bin",
+	RUST_PATH = vim.env.HOME .. "/.asdf/installs/rust/1.94.0/toolchains/1.94.0-aarch64-apple-darwin/bin",
 	JAVA_VERSION = java_version,
 	JAVA_HOME = vim.env.HOME .. "/.asdf/installs/java/" .. java_version,
 }
